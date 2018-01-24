6c27177bada6f27f085f325ec8ee9c89663e8465
Ivan Navarro <Ivan@Ivans-MacBook-Pro.local>
2018-01-24 16:49:32 +0100

    Generating documentaion

--- R/MyWeibull.R
+++ R/MyWeibull.R
@@ -1,5 +1,26 @@
-## PDF of Weibull distribution
+#' PDF of Weibull distribution
+#'
+#' This function calculates the probability density for x given ‘a’
+#' as the shape parameter and ‘b’as the scale parameter.
+#'
+#' @param x value.
+#' @param a Shape parameter.
+#' @param b Scale parameter.
+#' @return Probability density.
+#'
+#' @export
 MyWpdf <- function(x,a,b) if (x>=0) (a/b) * (x/b)^(a-1) * exp(-(x/b)^a) else 0
 
-### CDF of Weibull distribution
+
+#' CDF of Weibull distribution
+#'
+#' This function calculates the cumulative density for x given ‘a’
+#' as the shape parameter and ‘b’as the scale parameter.
+#'
+#' @param x value.
+#' @param a Shape parameter.
+#' @param b Scale parameter.
+#' @return Cumulative density.
+#'
+#' @export
 MyWcdf <- function(x,a,b) if (x>=0) 1-exp((-x/b)^a) else 0
